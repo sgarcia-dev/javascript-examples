69ad87f28288231f0349fe3f9ec5d7948a671a3f
Sergei Edoardo Garcia Ortega <sergei.garcia.ortega@accenture.com>
2016-03-03 17:08:30 -0600

    uploaded new examples

--- ecmascript6/with-transpiler/gulpfile.js
+++ ecmascript6/with-transpiler/gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	babel = require('gulp-babel'),
+	plumber = require('gulp-plumber'),
+	exec = require('child_process').exec;
 
-gulp.task('default', function () {
-	return console.log('gulp is running!');
+gulp.task('dev', ['js'], function () {
+	gulp.watch('src/**/*.js', [ 'js' ]);
+});
+
+gulp.task('js', function() {
+	gulp.src('src/app.js')
+		.pipe(plumber())
+		.pipe(babel({ presets: ['es2015'] }))
+		.pipe(gulp.dest('dist'));
+	exec('npm start', function(error, stdout, stderror) {
+		console.log('stdout: ' + stdout);
+		if (stderror)
+			console.log('stderr: ' + stderror);
+		if (error)
+			console.log('error: ' + error);
+	});
 });
